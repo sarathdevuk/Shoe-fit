bd96d6ec1cae9bb6ce5e9d4edc97c7286e8eecea
Sarathdev <sarathdev0@gmail.com>
2023-03-22 21:28:36 +0530

    login error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const session = require('express-session')
 
-// const errorHandler = require('./middleware/errorHandler')
+const errorHandler = require('./middleware/errorHandler')
 
 const userRoutes = require("./routes/userRoutes")
 const adminRoutes = require("./routes/adminRoutes")
@@ -65,7 +65,7 @@ app.use("*", (req,res)=>{
   throw new Error("Page not found")
 })
 
-// app.use(errorHandler)
+app.use(errorHandler)
 // app.use('/product',productRoutes)
 
 app.listen(3000, () => {
